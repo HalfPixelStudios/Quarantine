dc143c53b3e57987bdcbf4cb23f1f9b63ec04532
Daniel Liu <mr.picklepinosaur@gmail.com>
2020-03-14 23:04:24 -0400

    some map ideas

---
+++ Assets/Maps/tilesets/COLLIDER.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="COLLIDER" tilewidth="8" tileheight="8" tilecount="1" columns="1">
+ <image source="../images/tile_COLLIDER.png" width="8" height="8"/>
+</tileset>
---
+++ Assets/Maps/tilesets/overgrown.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="overgrown" tilewidth="8" tileheight="8" tilecount="64" columns="8">
+ <image source="../images/melon_overgrown.png" width="64" height="64"/>
+</tileset>
